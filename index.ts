a9aa41ebdc6ec62db7ffb02852732e001666a505
Furtif <msntchat@hotmail.fr>
2024-03-22 01:09:22 +0100

    add     randomBytes,     randomFillSync Credits to Flechaa
    review createHash

--- index.ts
+++ index.ts
@@ -1,7 +1,11 @@
 import { Buffer } from "buffer";
 
+type BinaryToTextEncoding = "binary" | "base64" | "base64url" | "hex";
+
 export default {
     createHash,
+    randomBytes,
+    randomFillSync
 }
 
 export function createHash(type: ChecksumType): Hash {
@@ -16,34 +20,62 @@ export class Hash {
     }
 
     update(data: string | Buffer | DataView, inputEncoding?: string): Hash {
-        // TODO: TypedArray
-        if (data instanceof DataView)
-            throw new Error("DataView not yet supported");
-        if (inputEncoding !== undefined)
-            throw new Error("inputEncoding not yet supported");
-
-        if (data instanceof Buffer)
-            this.checksum.update(data.buffer as ArrayBuffer);
-        else
-            this.checksum.update(data);
-
+        if (data instanceof DataView) {
+            const uint8Array = new Uint8Array(data.buffer, data.byteOffset, data.byteLength);
+            this.checksum.update(uint8Array.buffer as ArrayBuffer);
+        }
+        if (inputEncoding !== undefined) {
+            this.checksum.update(data as string);
+        } else {
+            if (data instanceof Buffer) {
+                this.checksum.update(data.buffer as ArrayBuffer);
+            } else {
+                this.checksum.update(data as string);
+            }
+        }
         return this;
     }
 
     digest(encoding: BinaryToTextEncoding = "binary"): Buffer | string {
         if (encoding === "hex")
             return this.checksum.getString();
-
         const rawDigest = Buffer.from(this.checksum.getDigest());
         if (encoding === "binary")
             return rawDigest;
-
         return rawDigest.toString(encoding);
     }
 
     copy(): Hash {
-        throw new Error("copy() not yet supported");
+        return new Hash(this.checksum);
     }
 }
 
-type BinaryToTextEncoding = "binary" | "base64" | "base64url" | "hex";
+export function randomBytes(size: number, callback?: (err: Error | null, buf: Buffer) => void): Buffer | void {
+    if (callback && typeof callback === 'function') {
+        const bytes = Buffer.allocUnsafe(size);
+        for (let i = 0; i < size; i++)
+            bytes[i] = Math.floor(Math.random() * 256);
+        callback(null, bytes);
+    } else {
+        const bytes = Buffer.allocUnsafe(size);
+        for (let i = 0; i < size; i++)
+            bytes[i] = Math.floor(Math.random() * 256);
+        return bytes;
+    }
+}
+
+export function randomFillSync(buffer: Buffer | Uint8Array | DataView, offset = 0, size?: number): Buffer | Uint8Array | DataView {
+    if (!size) size = buffer.byteLength - offset;
+    if (offset < 0 || size < 0 || offset + size > buffer.byteLength)
+        throw new RangeError("Invalid offset or size");
+    if (buffer instanceof Buffer || buffer instanceof Uint8Array) {
+        for (let i = offset; i < offset + size; i++)
+            buffer[i] = Math.floor(Math.random() * 256);
+    } else if (buffer instanceof DataView) {
+        const uint8Array = new Uint8Array(buffer.buffer, buffer.byteOffset, buffer.byteLength);
+        for (let i = offset; i < offset + size; i++)
+            uint8Array[i] = Math.floor(Math.random() * 256);
+        return buffer;
+    }
+    return buffer;
+}
